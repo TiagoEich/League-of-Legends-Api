988476d0899c65fb59b773ef83d54e312f0f4bd5
TiagoEich <tiagobackeseichh@gmail.com>
2025-04-14 14:01:13 -0300

    Adding a post, delete and get mapping

--- src/main/java/com/lol/champs_info/controller/ChampionController.java
+++ src/main/java/com/lol/champs_info/controller/ChampionController.java
@@ -1,10 +1,10 @@
 package com.lol.champs_info.controller;
 
+import com.lol.champs_info.model.ChampionEntity;
 import com.lol.champs_info.service.ChampionService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,4 +23,33 @@ public class ChampionController {
     return ResponseEntity.ok(championService.getChampions());
 
     }
+
+    @GetMapping("/region/{region}")
+    public ResponseEntity<List<ChampionEntity>> getChampionsByRegion(@PathVariable String region) {
+        return ResponseEntity.ok(championService.getChampionsFromRegion(region));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createChampion(@RequestBody ChampionEntity champion) {
+        try {
+            ChampionEntity createdChampion = championService.addChampion(champion);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdChampion);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+
+
+//    @DeleteMapping("/{id}") THIS METHOD HAS TO COMMENTED BECAUSE THE USER CAN'T AND WON'T SEE THE IDS OF THE CHAMPIONS
+        //SO I MUST ONLY USE IT WHEN I HAVE A DUPLICATE CHAMPION NAME WITH THE SAME ROLE.
+//    public ResponseEntity<ChampionEntity> deleteById(@PathVariable UUID id) {
+//        boolean isDeleted = championService.deleteById(id);
+//
+//        if (isDeleted) {
+//            return ResponseEntity.ok().build();
+//        } else {
+//            return ResponseEntity.notFound().build();
+//        }
+//    }
 }
