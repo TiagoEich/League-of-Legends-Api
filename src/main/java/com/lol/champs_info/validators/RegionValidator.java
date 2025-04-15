47fb675ad98d6261e16e4598b3dca5e53e1ec75a
TiagoEich <tiagobackeseichh@gmail.com>
2025-04-15 17:36:47 -0300

    Adding RegionValidator

---
+++ src/main/java/com/lol/champs_info/validators/RegionValidator.java
@@ -0,0 +1,27 @@
+package com.lol.champs_info.validators;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class RegionValidator {
+    public static final List <String> VALID_REGIONS = List.of(
+            "Bandle City",
+            "Bilgewater", "Demacia", "Freljord", "Ionia",
+            "Ixtal", "Noxus", "Piltover", "Runeterra",
+            "Shadow Isles", "Shurima", "Targon", "The Void", "Zaun"
+    );
+    public void validateRegion (String region) {
+        if (region== null || region.trim().isEmpty()) {
+            throw new IllegalArgumentException("Region is required");
+        }
+
+        boolean isValid = VALID_REGIONS.stream()
+                .anyMatch(validRegion -> validRegion.equalsIgnoreCase(region));
+        if (!VALID_REGIONS.contains(region.toUpperCase())) {
+            throw new IllegalArgumentException("Invalid region: '" +region+ "'. Valid regions are: '"+ VALID_REGIONS + "'");
+        }
+    }
+}
+/* */
