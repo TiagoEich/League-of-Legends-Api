88268bb149a429d2dc963f3a1d5cf58151523db6
TiagoEich <tiagobackeseichh@gmail.com>
2025-04-15 17:42:03 -0300

    Adding deleteByNameAndRole method. Updated getChampionsByRole, getChampionsByClass and getChampionsFromRegion on Controller

--- src/main/java/com/lol/champs_info/controller/ChampionController.java
+++ src/main/java/com/lol/champs_info/controller/ChampionController.java
@@ -18,20 +18,29 @@ public class ChampionController {
     }
 
     @GetMapping("/names")
-        public ResponseEntity<List<String>> getAllChampsName() {
-    return ResponseEntity.ok(championService.getChampionsName());
+    public ResponseEntity<List<String>> getAllChampsName() {
+        return ResponseEntity.ok(championService.getChampionsName());
 
     }
 
     @GetMapping("/region/{region}")
-    public ResponseEntity<List<ChampionEntity>> getChampionsByRegion(@PathVariable String region) {
-        return ResponseEntity.ok(championService.getChampionsFromRegion(region));
+    public ResponseEntity<?> getByRegion(@RequestParam String region) {
+        try {
+            return ResponseEntity.ok(championService.getChampionsFromRegion(region));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
 
+
     @GetMapping("/class/{classType}")
-    public ResponseEntity<List<ChampionEntity>>getChampionsByClass(@PathVariable String classType) {
-        return ResponseEntity.ok(championService.getChampionsByClass(classType));
+    public ResponseEntity<?>getChampionsByClass(@PathVariable String classType) {
+        try{
+            return ResponseEntity.ok(championService.getChampionsByClass(classType));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
     @GetMapping("/role/{role}")
@@ -54,7 +63,7 @@ public class ChampionController {
         }
     }
 
-    @PostMapping
+    @PostMapping("/addChampion")
     public ResponseEntity<?> createChampion(@RequestBody ChampionEntity champion) {
         try {
             ChampionEntity createdChampion = championService.addChampion(champion);
@@ -64,14 +73,29 @@ public class ChampionController {
         }
     }
 
+    @PutMapping("/champions")
+    public ResponseEntity<ChampionEntity> updateChampion(@RequestBody ChampionEntity updatedChampion) {
+        ChampionEntity result = championService.updateChampion(updatedChampion);
+        if (result == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(result);
+    }
 
+    @DeleteMapping("/deleteChampion")
+    public ResponseEntity<String> deleteByNameAndRole (@RequestParam String name, @RequestParam String role) {
+        boolean deleted = championService.deleteByNameAndRole(name, role);
 
-
-
-
+        if (deleted) {
+            return ResponseEntity.ok("Champion with name '"+name+"' and role '"+role+"' was deleted successfully");
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 //    @DeleteMapping("/{id}") THIS METHOD HAS TO COMMENTED BECAUSE THE USER CAN'T AND WON'T SEE THE IDS OF THE CHAMPIONS
-        //SO I MUST ONLY USE IT WHEN I HAVE A DUPLICATE CHAMPION NAME WITH THE SAME ROLE.
+    //SO I MUST ONLY USE IT WHEN I HAVE A DUPLICATE CHAMPION NAME WITH THE SAME ROLE.
 //    public ResponseEntity<ChampionEntity> deleteById(@PathVariable UUID id) {
 //        boolean isDeleted = championService.deleteById(id);
 //
